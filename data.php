3fa78908a368fb5f9054384c08c58cb153f98a78
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-15 23:02:23 -0400

    Create data.php

---
+++ data.php
@@ -0,0 +1,34 @@
+<?php
+
+function get_price($name, $price)
+{
+
+
+  $host = "tcp:localhost01.database.windows.net,1433";
+	$user = "pilakag1@localhost01";
+	$pwd  = "Pilakag_1";
+	$db   = "pilakag1_db";
+  $conn = new PDO("sqlsrv:Server = $host; Database = $db", $user, $pwd);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $sql = "SELECT price FROM product WHERE product = '$name'";
+       
+  $stmt = $conn->query("$sql");
+	$row = $stmt->fetch();
+	$p = $row["price"];
+	$conn = NULL;
+  
+  if ($p == $price) 
+  {
+    return "true";
+  }
+else 
+ {
+  return "false";
+
+ }
+}
+
+?>
+
+
